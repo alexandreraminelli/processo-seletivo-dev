bf6605cbf85979e3ecb9250a7a324c376fd13103
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-18 15:16:43 -0300

    desafio concluído

--- index.ts
+++ index.ts
@@ -3,19 +3,19 @@ import { PostFilter, search } from "./search"
 async function main() {
   // Você pode essa função à vontade também pra testar!
   const filter: PostFilter = {
-    title: '9',
-    kind: 'report',
-    category: 'Notícia',
-    target: 'Aluno',
-    publishedAtStart: new Date('2024-01-01'),
-    publishedAtEnd: new Date('2024-06-20'),
-    createdAtStart: new Date('2023-08-01'),
-    createdAtEnd: new Date('2023-12-20'),
+    // title: "875",
+    // kind: "informative",
+    // category: "Tarefa",
+    // target: "Coordenador",
+    // publishedAtStart: new Date("2024-03-24"),
+    // publishedAtEnd: new Date('2024-06-20'),
+    // createdAtStart: new Date("2023-10-21"),
+    // createdAtEnd: new Date("2023-11-21"),
   }
 
-  console.time('filter')
+  console.time("filter")
   const posts = await search(filter)
-  console.timeEnd('filter')
+  console.timeEnd("filter")
 
   console.table(posts)
 }
--- search.ts
+++ search.ts
@@ -1,9 +1,9 @@
-import { readFileSync } from 'fs'
+import { readFileSync } from "fs"
 
 export type Post = {
   id: number
   title: string
-  kind: 'report' | 'gallery' | 'video' | 'banner' | 'informative'
+  kind: "report" | "gallery" | "video" | "banner" | "informative"
   publishedAt: string
   createdAt: string
   category: string[]
@@ -12,7 +12,7 @@ export type Post = {
 
 export type PostFilter = {
   title?: string
-  kind?: 'report' | 'gallery' | 'video' | 'banner' | 'informative'
+  kind?: "report" | "gallery" | "video" | "banner" | "informative"
   category?: string
   target?: string
   publishedAtStart?: Date
@@ -22,12 +22,29 @@ export type PostFilter = {
 }
 
 export async function search(filter: PostFilter): Promise<Post[]> {
-  const posts: Post[] = JSON.parse(readFileSync('./data/posts.json', 'utf-8'))
+  const posts: Post[] = JSON.parse(readFileSync("./data/posts.json", "utf-8"))
 
   /********************************************
    *            CODIFIQUE AQUI!
    *    Você deve implementar essa função
    ********************************************/
+  let result: Post[] = posts
 
-  return posts
+  // Filtrar por título
+  if (filter.title) result = result.filter((post) => post.title.toLowerCase().includes(filter.title!.toLowerCase()))
+  // Filtrar por tipo
+  if (filter.kind) result = result.filter((post) => post.kind.includes(filter.kind!))
+  // Filtrar por categoria
+  if (filter.category) result = result.filter((post) => post.category.includes(filter.category!))
+  // Filtrar por tags
+  if (filter.target) result = result.filter((post) => post.targets.includes(filter.target!))
+
+  // Filtrar por data de publicação
+  if (filter.publishedAtStart) result = result.filter((post) => new Date(post.publishedAt) >= filter.publishedAtStart!)
+  if (filter.publishedAtEnd) result = result.filter((post) => new Date(post.publishedAt) <= filter.publishedAtEnd!)
+  // Date de criação
+  if (filter.createdAtStart) result = result.filter((post) => new Date(post.createdAt) >= filter.createdAtStart!)
+  if (filter.createdAtEnd) result = result.filter((post) => new Date(post.createdAt) <= filter.createdAtEnd!)
+
+  return result.sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime())
 }
